f111782da4803ff3a697e6a62cbb79845b0775e2
elmrabet.jaafar <elmrabet.jaafar@gmail.com>
2024-11-06 13:58:10 +0100

    add seo

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://www.borne-kébé.com/sitemap.xml',
+    sitemap: 'https://www.borne-kébè.com/sitemap.xml',
   }
 } 
