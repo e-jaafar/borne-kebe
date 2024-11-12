4ab29cf2d003aebd9601fcecbe920f17c660b375
elmrabet.jaafar <elmrabet.jaafar@gmail.com>
2024-11-12 22:03:54 +0100

    update robot favicon

--- app/robots.ts
+++ app/robots.ts
@@ -12,8 +12,6 @@ export default function robots(): MetadataRoute.Robots {
           '/api/',
           '/_next/',
           '/private/',
-          '*.json',
-          '*.xml',
           '/images/',
           '/og-image.jpg',
           '/favicon.ico'
